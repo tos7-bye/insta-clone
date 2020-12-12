c6a9a6d3b2c694cd158372e9ce25ba2db2990aae
TT <tos.10041105@icloud.com>
2020-12-13 00:10:49 +0900

    finish signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+# GET/users/:id 
+  def show
+    @user = User.find(params[:id])
+  end
+# GET/users/new
   def new
+    @user = User.new
+  end
+# POST/users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to the Inata Clone!"
+      # success(valid params)
+    else
+      # Failure(not valid params)
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                                :password_cofirmation)
   end
 end
