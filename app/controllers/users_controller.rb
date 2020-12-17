0a8e0a3694d3a49724e9e95b871d7a6dd993015b
TT <tos.10041105@icloud.com>
2020-12-17 12:39:55 +0900

    finsh account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      redirect_to @user
-      flash[:success] = "Welcome to the Inata Clone!"
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
       # success(valid params)
     else
       # Failure(not valid params)
