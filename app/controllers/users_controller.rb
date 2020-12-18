305cd21f1048e3b41731afe8555f4f0ef10a77ce
TT <tos.10041105@icloud.com>
2020-12-18 22:51:27 +0900

    finish microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 # GET/users/:id 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 # GET/users/new
   def new
@@ -55,14 +56,6 @@ class UsersController < ApplicationController
   end
 
     # beforeアクション
-   # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
 
   # 正しいユーザーかどうか確認
   def correct_user
